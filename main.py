323e8d5cbc77df80ad53355a9228fbcfd0e483ea
LemanRus <wlad163163@gmail.com>
2023-09-11 21:13:54 +0400

    Add timeout for serial port response

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import serial.serialutil as sutil
 port = input("Введите номер COM-порта: ")
 
 try:
-    with serial.Serial(port, baudrate=9600, bytesize=8, stopbits=1, parity=serial.PARITY_NONE) as ser:
+    with serial.Serial(port, baudrate=9600, bytesize=8, stopbits=1, parity=serial.PARITY_NONE, timeout=60) as ser:
         filename = input("Введите имя файла (без расширения) для выходных данных: ")
 
         with open(filename + ".txt", 'a') as output_file:
@@ -12,6 +12,8 @@ try:
                 try:
                     line = ser.read_until(expected=sutil.CR)
                     output_file.write(line)
+                except serial.SerialTimeoutException:
+                    break
                 except KeyboardInterrupt:
                     break
 
