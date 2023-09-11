c1cd83b54b32bab198977977cb0d836ebea58440
LemanRus <wlad163163@gmail.com>
2023-09-11 21:04:57 +0400

    Add basic read from serial port with writing to file

---
+++ main.py
@@ -0,0 +1,22 @@
+import serial
+import serial.serialutil as sutil
+
+port = input("Введите номер COM-порта: ")
+
+try:
+    with serial.Serial(port, baudrate=9600, bytesize=8, stopbits=1, parity=serial.PARITY_NONE) as ser:
+        filename = input("Введите имя файла (без расширения) для выходных данных: ")
+
+        with open(filename + ".txt", 'a') as output_file:
+            while True:
+                try:
+                    line = ser.read_until(expected=sutil.CR)
+                    output_file.write(line)
+                except KeyboardInterrupt:
+                    break
+
+except serial.SerialException:
+    print(f"Порт {port} недоступен")
+
+
+
